2f40562c08adad3c54194e81ae448df39952c6b1
THESHARK9001 <76677889+THESHARK9001@users.noreply.github.com>
2025-04-11 05:16:35 -0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,8 +1,4 @@
 @echo Please be sure you have python 3.13 before building and that you have run "pip install -r requirements.txt" or this will not work
 @pause
 @pyinstaller --noconsole --clean --onefile --icon=".\ai-build\chatgpt.ico" .\ai-build\gpt.pyw
-@del gpt.spec
-@y|rmdir /s build
-@copy .\dist\gpt.exe .\gpt.exe
-@y|rmdir /s dist
 @pause
